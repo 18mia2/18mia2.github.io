0d05b0686a732ad52d7ef983cbafdde2c0afd046
18mia2 <59624355+18mia2@users.noreply.github.com>
2020-01-08 11:13:14 -0500

    Update contact.php

--- modal/contact.php
+++ modal/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // Put contacting email here
-$php_main_email = "maram.assi@queensu.ca";
+$php_main_email = "assi.maram@gmail.com";
 
 //Fetching Values from URL
 $php_name = $_POST['ajax_name'];
